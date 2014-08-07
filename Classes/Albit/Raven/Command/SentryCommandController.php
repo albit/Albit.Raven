7d7653107ef850598ae9f23ed62d91ca00130d4a
Benjamin Albrecht <benjamin.albrecht@albit.de>
2014-08-07 11:57:26 +0200

    [TASK] adjust output styling

--- Classes/Albit/Raven/Command/SentryCommandController.php
+++ Classes/Albit/Raven/Command/SentryCommandController.php
@@ -23,9 +23,9 @@ class SentryCommandController extends \TYPO3\Flow\Cli\CommandController {
 	public function testConfigurationCommand() {
 		$this->response->setOutputFormat(\TYPO3\Flow\Cli\Response::OUTPUTFORMAT_STYLED);
 		if (isset($this->sentryDsn) && strlen($this->sentryDsn) > 0) {
-			$this->response->appendContent('<p>Used Sentry DSN: ' . $this->sentryDsn . '</p>');
+			$this->response->appendContent('Used Sentry DSN: ' . $this->sentryDsn . PHP_EOL);
 		} else {
-			$this->response->appendContent('<p>No Sentry DSN configured</p>');
+			$this->response->appendContent('No Sentry DSN configured' . PHP_EOL);
 		}
 	}
 
